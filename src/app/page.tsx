7290ccb291509fd32b0fd374b4a14e870614ec56
Saira-kh1 <sairakhalilahme@gmail.com>
2025-11-17 09:30:50 +0500

    02: customization-just imported checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from  "@/components/ui/textarea";
-import { Checkbox } from "@radix-ui/react-checkbox";
+import { Checkbox } from "@/components/ui/checkbox";
 export default function Home() {
   return (
     <div className="p-4">
