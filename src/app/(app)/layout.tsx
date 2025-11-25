f651b813524718565e1dfe11f84b6b6b410f6a04
Saira-kh1 <sairakhalilahme@gmail.com>
2025-11-25 14:00:19 +0500

    07: trpc-integration

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@
 
 import type { Metadata } from "next";
 import { DM_Sans} from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/client";
 import "./globals.css";
 
 const dmSans = DM_Sans({
@@ -24,7 +25,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${dmSans.className}  antialiased`}>
-        {children}
+        <TRPCReactProvider>
+          {children}
+        </TRPCReactProvider>
+        
       </body>
     </html>
   );
