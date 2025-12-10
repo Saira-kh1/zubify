1f84ae69e5cc35d1a38a9b066798488299177a08
Saira-kh1 <sairakhalilahme@gmail.com>
2025-12-10 15:52:06 +0500

    cart and checkout

---
+++ src/modules/checkout/store/use-cart-store.ts
@@ -0,0 +1,73 @@
+import { create } from 'zustand';
+import {createJSONStorage, persist } from 'zustand/middleware';
+
+
+interface TenantCart {
+    productIds: String[];
+
+};
+
+interface CartState {
+    tenantCarts: Record<string, TenantCart>;
+    addProduct: (tenantSlug: string, productId: string) => void;
+    removeProduct: (tenantSlug: string, productId: string) => void;
+    clearCart: (tenantSlug: string) => void;
+    clearAllCarts: () => void;
+    getCartByTenant: (tenantSlug: string) => String[];
+};
+
+
+export const useCartStore = create<CartState>()(
+
+    persist(
+        ( set , get) => ({
+            tenantCarts: {},
+            addProduct: (tenantSlug, productId) =>
+                set((state)=>({
+                    tenantCarts: {
+                        ...state.tenantCarts,
+                        [tenantSlug]: {
+                            productIds:[
+                                ...(state.tenantCarts[tenantSlug]?.productIds || []),
+                                productId,
+                            ]
+                    }
+                }
+        })),
+            removeProduct: (tenantSlug, productId) =>
+                set((state) => ({
+                        tenantCarts: {
+                            ...state.tenantCarts,
+                            [tenantSlug]: {
+                                productIds: state.tenantCarts[tenantSlug]?.productIds.filter(
+                                    (id) => id !== productId
+                            )|| [],
+                        }
+                    }
+        })),
+            clearCart: (tenantSlug) =>
+                set((state) => ({
+                        tenantCarts: {
+                            ...state.tenantCarts,
+                            [tenantSlug]: {
+                                productIds: [],
+                        },
+                    },
+                
+        })),
+            clearAllCarts: () =>
+                set({
+                        tenantCarts: {},
+                    }),
+                    getCartByTenant: (tenantSlug) =>
+                     get().tenantCarts[tenantSlug]?.productIds || [],   
+                
+        }),
+            
+        {
+            name: "ZuBiFy-cart",
+            storage: createJSONStorage(() => localStorage),
+        },
+
+   ),
+);
--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -2,6 +2,8 @@
 
 //TODO: Add real ratings 
 import { formatCurrency, generateTenantURL } from "@/lib/utils";
+
+import dynamic from "next/dynamic";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import  Link  from "next/link";
@@ -11,6 +13,19 @@ import { Button } from "@/components/ui/button";
 import { LinkIcon ,StarIcon} from "lucide-react";
 import { Fragment } from "react";
 import { Progress } from "@/components/ui/progress";
+// import { CartButton } from "../components/cart-button";
+
+const CartButton = dynamic(
+    () => import("../components/cart-button").then(
+        (mod) => mod.CartButton,
+    ),
+    {
+        ssr: false,
+        loading: () => <Button disabled className="flex-1 bg-pink-400">Add to cart</Button>
+            
+        },
+    
+);
 
 
 interface ProductViewProps {
@@ -100,12 +115,11 @@ export const ProductView =({ productId , tenantSlug}: ProductViewProps) =>{
                         <div className="border-t lg:border-t-0 lg:border-l h-full">
                             <div className="flex flex-col gap-4 p-6 border-b">
                                 <div className="flex flex-row items-center gap-2">
-                                    <Button 
-                                     variant="elevated"
-                                     className="flex-1 bg-pink-400"
-                                    >
-                                        Add to Cart
-                                    </Button>
+                                  <CartButton 
+                                  productId={productId}
+                                  tenantSlug={tenantSlug}
+                                  
+                                  />
                                     <Button
                                     className="size-12"
                                     variant="elevated"
